8dc244ea20f6dc3cdd266646e8d16e06e86bb006
Wilson <wtovaragudelo@gmail.com>
2024-10-06 23:22:44 +0200

    feat: noto sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   content: ['./resources/**/*.edge', './resources/**/*.{js,ts,jsx,tsx,vue}'],
 
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Noto Sans, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
   },
 
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
